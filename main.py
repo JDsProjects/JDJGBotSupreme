9b9d2bf664cd53139dc2618bdeed31781400e1b1
JDJG Inc. Official <ghostzap123@gmail.com>
2022-02-14 23:55:37 +0000

    ok

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from discord.ext import commands
 import discord
 import os
+import B
+import logging 
+
+bot = commands.Bot(command_prefix = "jd/", intents = discord.Intents.all())
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        try:
+            bot.load_extension(f"cogs.{filename[:-3]}")
+        except commands.errors.ExtensionError:
+            traceback.print_exc()
+
+logging.basicConfig(level=logging.INFO)
+B.b()
+bot.run(os.environ["TOKEN"])
