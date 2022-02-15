7d23784c863f6e6b622c470296af40a09242d530
JDJG Inc. Official <ghostzap123@gmail.com>
2022-02-15 00:55:53 +0000

    alr cool

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ import os
 import B
 import logging 
 import traceback
+import re
 
-bot = commands.Bot(command_prefix = "jd/", intents = discord.Intents.all())
+async def get_prefix(bot, message):
+    extras = ["jd/", "j/"]
+
+    comp = re.compile("^(" + "|".join(map(re.escape, extras)) + ").*", flags=re.I)
+    match = comp.match(message.content)
+    if match is not None:
+        extras.append(match.group(1))
+
+    return commands.when_mentioned_or(*extras)(bot, message)
+
+bot = commands.Bot(command_prefix = (get_prefix), intents = discord.Intents.all())
 
 @bot.event
 async def on_error(event, *args, **kwargs):
