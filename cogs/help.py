71e9162d86b890e71cecb1ce281dbead73fc0336
pieckenst <rsncra@gmail.com>
2022-02-15 20:09:14 +0300

    WIP help subclass

--- cogs/help.py
+++ cogs/help.py
@@ -4,17 +4,18 @@ from discord.ext import commands
 from discord.ui import Button, Select, View
 
 
+
 class Dropdown(discord.ui.Select):
-    def __init__(self, bot):
-        self.bot = bot  # one thing fixed...
+    def __init__(self,options,ctx):
+        self.bot = ctx.bot  # one thing fixed...
 
         # Set the options that will be presented inside the dropdown
-        options = [
+        #options = [
         #THIS IS PURE PLACEHOLDER , PLEASE EDIT TO WHATEVER THE FINAL BOT WILL HAVE
-            SelectOption(label="Placeholder", value="Placeholder"),
+            #SelectOption(label="Placeholder", value="Placeholder"),
             
-            SelectOption(label="Close", value="Close"),
-        ]
+            #SelectOption(label="Close", value="Close"),
+        #]
 
         # The placeholder is what will be shown when no option is chosen
         # The min and max values indicate we can only pick one of the three options
@@ -84,7 +85,7 @@ async def get_help(self, interaction, CogToPassAlong):
     await interaction.response.edit_message(embed=emb)
 
 
-class Help(commands.Cog):
+"""class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -109,8 +110,109 @@ class Help(commands.Cog):
         # async def callback(interaction):
         # await interaction.send(embed=embed)
 
-        await ctx.send(embed=embede, view=view)
+        await ctx.send(embed=embede, view=view)"""
 
+class Help(commands.Cog):
+    "The Help Menu Cog"
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+        self.bot.help_command = MyHelp()
+        
+
+class HelpEmbed(discord.Embed): # Our embed with some preset attributes to avoid setting it multiple times
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.timestamp = discord.utils.utcnow()
+        text = "Use help [command] or help [category] for more information | <> is required | [] is optional"
+        self.set_footer(text=text)
+        self.color = discord.Color.blurple()
+
+class MyHelp(commands.HelpCommand):
+    def __init__(self):
+     
+     super().__init__(command_attrs={
+                "help": "The help command for the bot"
+            })# create our class with some aliases and cooldown
+            
+        
+    
+    async def send(self, **kwargs):
+        """a short cut to sending to get_destination"""
+        await self.get_destination().send(**kwargs)
+
+    async def send_bot_help(self, mapping):
+        """triggers when a `<prefix>help` is called"""
+        ctx = self.context
+        embed = HelpEmbed(title=f"{ctx.me.display_name} Help")
+        #embed.set_thumbnail(url=ctx.me.avatar)
+        usable = 0 
+        myoptions = []
+        view = DropdownView(myoptions,ctx)
+
+        for cog, commands in mapping.items(): #iterating through our mapping of cog: commands
+            if filtered_commands := await self.filter_commands(commands): 
+                # if no commands are usable in this category, we don't want to display it
+                amount_commands = len(filtered_commands)
+                usable += amount_commands
+                if cog: # getting attributes dependent on if a cog exists or not
+                    name = cog.qualified_name
+                    description = cog.description or "No description"
+                else:
+                    name = "No Category"
+                    description = "Commands with no category"
+
+                #embed.add_field(name=f"{name} Category [{amount_commands}]", value=description)
+                myoptions.append(SelectOption(label=name, value=name))
+
+        #embed.description = f"{len(bot.commands)} commands | {usable} usable" 
+        myoptions.append(SelectOption(label="Close", value="Close"))
+
+        await self.send(embed=embed,view=view)
+
+    async def send_command_help(self, command):
+        """triggers when a `<prefix>help <command>` is called"""
+        signature = self.get_command_signature(command) # get_command_signature gets the signature of a command in <required> [optional]
+        embed = HelpEmbed(title=signature, description=command.help or "No help found...")
+
+        if cog := command.cog:
+            embed.add_field(name="Category", value=cog.qualified_name)
+
+        can_run = "No"
+        # command.can_run to test if the cog is usable
+        with contextlib.suppress(commands.CommandError):
+            if await command.can_run(self.context):
+                can_run = "Yes"
+            
+        embed.add_field(name="Usable", value=can_run)
+
+        if command._buckets and (cooldown := command._buckets._cooldown): # use of internals to get the cooldown of the command
+            embed.add_field(
+                name="Cooldown",
+                value=f"{cooldown.rate} per {cooldown.per:.0f} seconds",
+            )
+
+        await self.send(embed=embed)
+
+    async def send_help_embed(self, title, description, commands): # a helper function to add commands to an embed
+        embed = HelpEmbed(title=title, description=description or "No help found...")
+
+        if filtered_commands := await self.filter_commands(commands):
+            for command in filtered_commands:
+                embed.add_field(name=self.get_command_signature(command), value=command.help or "No help found...")
+           
+        await self.send(embed=embed)
+
+    async def send_group_help(self, group):
+        """triggers when a `<prefix>help <group>` is called"""
+        title = self.get_command_signature(group)
+        await self.send_help_embed(title, group.help, group.commands)
+
+    async def send_cog_help(self, cog):
+        """triggers when a `<prefix>help <cog>` is called"""
+        title = cog.qualified_name or "No"
+        await self.send_help_embed(f'{title} Category', cog.description, cog.get_commands())
 
 def setup(bot):
     bot.add_cog(Help(bot))
