93739a108e5bae174c1523036edcc369a1b8d06e
JDJG Inc. Official <ghostzap123@gmail.com>
2022-02-15 00:25:10 +0000

    Jsk doesn't work?

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ import traceback
 
 bot = commands.Bot(command_prefix = "jd/", intents = discord.Intents.all())
 
+@bot.event
+async def on_error(event, *args, **kwargs):
+  more_information = os.sys.exc_info()
+  error_wanted = traceback.format_exc()
+  traceback.print_exc()
+  #print(more_information[0])
+
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         try:
