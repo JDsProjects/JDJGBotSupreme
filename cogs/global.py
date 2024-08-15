159c72fee134a4dabda3847eb90ef0312c3c4bb0
JDJG <ghostzap123@gmail.com>
2024-08-14 21:11:28 -0400

    There

--- cogs/global.py
+++ cogs/global.py
@@ -1,1 +1,14 @@
+from discord.ext import commands
+
 # area to start designing global chat.
+
+
+class Global(commands.Cog):
+    "Global Chat commands"
+
+    def __init__(self, bot):
+        self.bot = bot
+
+async def setup(bot):
+    await bot.add_cog(Global(bot))
+
